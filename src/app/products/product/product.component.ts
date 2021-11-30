701351c1bf36f27e47a9a9c5cdb68c097ab0604f
Kevin <bastokevindaniel@gmail.com>
2021-11-30 01:44:29 -0600

    products form

--- src/app/products/product/product.component.ts
+++ src/app/products/product/product.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Product } from '../entities/product';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-product',
@@ -9,9 +12,13 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class ProductComponent implements OnInit {
 
   public productForm : FormGroup;
+  private productId : string;
+  private product : Product
 
   constructor(
-    private fb : FormBuilder
+    private fb : FormBuilder,
+    private router : Router,
+    private productsService : ProductsService
   ) {
     this.productForm = this.fb.group({
       name : [""],
@@ -22,6 +29,55 @@ export class ProductComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.productId = this.router.url.split("/")[this.router.url.split("/").length - 1];
+    if(this.productId != "new"){
+      this.productsService.fetchProduct(this.productId)
+      .then((result) => {
+        this.product = result;
+        this.setProductValues();
+      }).catch((err) => {
+        
+      });
+    }
   }
 
+  setProductValues(){
+    this.productForm.get('name').setValue(this.product.name);
+    this.productForm.get('description').setValue(this.product.description);
+    this.productForm.get('price').setValue(this.product.price);
+    this.productForm.get('picture').setValue(this.product.picture);
+  }
+
+  goBack(){
+    this.router.navigate([`/products`])
+  }
+
+  deleteProduct(){
+    if(this.productId != "new")
+      this.productsService.deleteProduct(this.productId)
+      .then((result) => {
+        this.router.navigate(['/products'])
+      }).catch((err) => {
+        
+      });
+  }
+
+  submit(){
+    let productData = this.productForm.value;
+    if(this.productId != "new"){
+      this.productsService.updateProduct(this.productId, productData)
+      .then((result) => {
+        this.router.navigate(['/products'])
+      }).catch((err) => {
+        
+      });
+    }else{
+      this.productsService.createProduct(productData)
+      .then((result) => {
+        this.router.navigate(['/products'])
+      }).catch((err) => {
+        
+      });
+    }
+  }
 }
--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { take } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
+import { CreateProduct } from './dtos/create-product';
+import { UpdateProduct } from './dtos/update-product';
 import { Product } from './entities/product';
 
 @Injectable({
@@ -13,7 +15,23 @@ export class ProductsService {
     private http : HttpClient
   ) { }
 
+  createProduct(product: CreateProduct) {
+    return this.http.post(`${environment.apiUrl}/products`, product).pipe(take(1)).toPromise();
+  }
+
   fetchProducts() : Promise<Array<Product>>{
     return this.http.get<Array<Product>>(`${environment.apiUrl}/products`).pipe(take(1)).toPromise();
+  }
+
+  fetchProduct(id : string) : Promise<Product>{
+    return this.http.get<Product>(`${environment.apiUrl}/products/${id}`).pipe(take(1)).toPromise();
+  }
+
+  updateProduct(id : string, product : UpdateProduct){
+    return this.http.patch(`${environment.apiUrl}/products/${id}`, product).pipe(take(1)).toPromise();
+  }
+
+  deleteProduct(id : string){
+    return this.http.delete(`${environment.apiUrl}/products/${id}`).pipe(take(1)).toPromise();
   }
 }
