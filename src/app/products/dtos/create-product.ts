cb877f44b5ae56d1e88aa865ac4c0d409f2501e5
Kevin <bastokevindaniel@gmail.com>
2021-11-29 20:35:31 -0600

    products dto established

--- src/app/products/dtos/create-product.ts
+++ src/app/products/dtos/create-product.ts
@@ -1,2 +1,6 @@
 export class CreateProduct {
+    name : string;
+    description : string;
+    price : number;
+    picture : string;
 }
--- src/app/products/dtos/update-product.ts
+++ src/app/products/dtos/update-product.ts
@@ -1,2 +1,6 @@
 export class UpdateProduct {
+    name? : string;
+    description? : string;
+    price? : number;
+    picture? : string;
 }
