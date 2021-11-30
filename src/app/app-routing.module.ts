e04ef54c7ab4ad25c8db61b7213f5a05eca91eb9
Kevin <bastokevindaniel@gmail.com>
2021-11-29 20:48:07 -0600

    routing completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,47 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountsComponent } from './accounts/accounts.component';
+import { ProductsComponent } from './products/products.component';
+import { PurchasesComponent } from './purchases/purchases.component';
+import { RolesComponent } from './roles/roles.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'roles',
+    pathMatch: 'full'
+  },
+  {
+    path: 'roles',
+    component: RolesComponent
+  },
+  {
+    path: 'users',
+    component: UsersComponent
+  },
+  {
+    path: 'accounts',
+    component: AccountsComponent
+  },
+  {
+    path: 'products',
+    component: ProductsComponent
+  },
+  {
+    path: 'transactions',
+    component: TransactionsComponent
+  },
+  {
+    path: 'purchases',
+    component: PurchasesComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'roles'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
